33b4a4786454339952d3a84033158c34f495cdf8
HaiHardware <80758501+HaiHardware@users.noreply.github.com>
2021-04-23 20:22:00 +0700

    Delete Lab02 directory

--- Lab02/outputlogic.v
+++
@@ -1,9 +0,0 @@
-module outputlogic(out,in);
-	output wire out;
-	input wire [2:0] in;
-	wire [1:0]in_bar;
-	not not1(in_bar[1],in[1]);
-	not not2(in_bar[0],in[0]);
-	and and1(out,in[2],in_bar[1],in_bar[0]);
-	
-endmodule 
